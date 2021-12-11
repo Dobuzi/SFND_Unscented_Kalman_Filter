f5ccadc5c570979a9b268928088b9f424698fbf3
Dobuzi <kimjw7981@gmail.com>
2021-12-11 16:52:37 +0000

    step: fix atan error, status: submit 2

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -21,10 +21,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 10;
+  std_a_ = 1.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = M_PI/2;
+  std_yawdd_ = 1.0;
   
   /**
    * DO NOT MODIFY measurement noise values below.
@@ -77,13 +77,12 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    */
   if (!is_initialized_)
   {
-    double px, py, var_px, var_py;
+    double px, py, var_init1 = 1., var_init2 = 1000.;
+
     if (meas_package.sensor_type_ == MeasurementPackage::LASER)
     {
       px = meas_package.raw_measurements_(0);
       py = meas_package.raw_measurements_(1);
-      var_px = pow(std_laspx_, 2);
-      var_py = pow(std_laspy_, 2);
     }
     else
     {
@@ -91,16 +90,14 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
              phi = meas_package.raw_measurements_(1);
       px = rho * cos(phi);
       py = rho * sin(phi);
-      var_px = pow(std_radr_, 2);
-      var_py = pow(std_radr_, 2);
+      var_init1 = 3.;
     }
-
-    x_ << px, py, 2, 0, M_PI/6;
-    P_ << var_px, 0, 0, 0, 0,
-          0, var_py, 0, 0, 0,
-          0, 0, 25, 0, 0,
-          0, 0, 0, 1, 0,
-          0, 0, 0, 0, 4;
+    x_ << px, py, 0., 0., 0.;
+    P_ << var_init1, 0., 0., 0., 0.,
+          0., var_init1, 0., 0., 0.,
+          0., 0., var_init2, 0., 0.,
+          0., 0., 0., var_init2, 0.,
+          0., 0., 0., 0., var_init2;
     
     time_us_ = meas_package.timestamp_;
     is_initialized_ = true;
@@ -132,11 +129,11 @@ void UKF::Prediction(double delta_t) {
   VectorXd x_aug = VectorXd(n_aug_);
   MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
   MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
-  x_aug << x_, 0, 0;
-  P_aug.fill(0.0);
+  x_aug << x_, 0., 0.;
+  P_aug.fill(0.);
   P_aug.topLeftCorner(n_x_, n_x_) << P_;
-  P_aug.bottomRightCorner(n_aug_-n_x_, n_aug_-n_x_) << pow(std_a_, 2), 0,
-                                                       0, pow(std_yawdd_, 2);
+  P_aug.bottomRightCorner(n_aug_-n_x_, n_aug_-n_x_) << pow(std_a_, 2), 0.,
+                                                       0., pow(std_yawdd_, 2);
   MatrixXd A_aug = P_aug.llt().matrixL();
   MatrixXd x_aug_Mat = MatrixXd(n_aug_, n_aug_);
   
@@ -149,7 +146,7 @@ void UKF::Prediction(double delta_t) {
               x_aug_Mat - sqrt(lambda_+n_aug_) * A_aug;
 
   // Sigma Point Prediction
-  Xsig_pred_.fill(0.0);
+  Xsig_pred_.fill(0.);
   MatrixXd X_del = MatrixXd(n_x_, 1);
   MatrixXd X_noise = MatrixXd(n_x_, 1);
 
@@ -165,17 +162,17 @@ void UKF::Prediction(double delta_t) {
     {
       X_del << v * cos(yaw) * delta_t,
                v * sin(yaw) * delta_t,
-               0,
-               0,
-               0;
+               0.,
+               0.,
+               0.;
     }
     else
     {
       X_del << v / yawd * (sin(yaw+yawd*delta_t) - sin(yaw)),
                v / yawd * (-cos(yaw+yawd*delta_t) + cos(yaw)),
-               0,
+               0.,
                yawd * delta_t,
-               0;
+               0.;
     }
     X_noise << 0.5 * pow(delta_t, 2) * cos(yaw) * nu_a,
                0.5 * pow(delta_t, 2) * sin(yaw) * nu_a,
@@ -205,6 +202,8 @@ void UKF::Prediction(double delta_t) {
       x_(j) += weights_(i) * Xsig_pred_(j, i);
     }
   }
+  while (x_(3) >  M_PI) x_(3) -= 2. * M_PI;
+  while (x_(3) < -M_PI) x_(3) += 2. * M_PI;
 
   for (int i = 0; i < 2 * n_aug_ + 1; i++)
   {
@@ -225,13 +224,13 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
   
   // Set measurement error
   MatrixXd R = MatrixXd(2, 2);
-  R << pow(std_laspx_, 2), 0,
-      0, pow(std_laspy_, 2);
+  R << pow(std_laspx_, 2), 0.,
+      0., pow(std_laspy_, 2);
   
   // Measurement Function
   MatrixXd H = MatrixXd(2, 5);
-  H << 1, 0, 0, 0, 0,
-       0, 1, 0, 0, 0;
+  H << 1., 0., 0., 0., 0.,
+       0., 1., 0., 0., 0.;
 
   // Get Kalman Gain
   VectorXd y = meas_package.raw_measurements_ - H * x_;
@@ -240,6 +239,8 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   // Measurement Update
   x_ += K * y;
+  while (x_(3) >  M_PI) x_(3) -= 2. * M_PI;
+  while (x_(3) < -M_PI) x_(3) += 2. * M_PI;
   MatrixXd I = MatrixXd::Identity(n_x_, n_x_);
   P_ = (I - K * H) * P_;
 }
@@ -264,16 +265,16 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
            yaw = Xsig_pred_(3, i);
 
     Zsig(0, i) = sqrt(pow(px, 2) + pow(py, 2));
-    Zsig(1, i) = atan(py/px);
-    Zsig(2, i) = (px * cos(yaw) * v + py * sin(yaw) * v) / Zsig(0, i);
+    Zsig(1, i) = atan2(py, px);
+    Zsig(2, i) = (px * cos(yaw) + py * sin(yaw)) * v / Zsig(0, i);
   }
 
   z_pred = (Zsig * weights_).rowwise().sum();
 
   MatrixXd R = MatrixXd(n_z, n_z);
-  R << pow(std_radr_, 2), 0, 0,
-       0, pow(std_radphi_, 2), 0,
-       0, 0, pow(std_radrd_, 2);
+  R << pow(std_radr_, 2), 0., 0.,
+       0., pow(std_radphi_, 2), 0.,
+       0., 0., pow(std_radrd_, 2);
   
   MatrixXd S = MatrixXd(n_z, n_z);
   S.fill(0.0);
@@ -281,8 +282,8 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
   for (int i = 0; i < 2 * n_aug_ + 1; i++)
   {
     VectorXd z_diff = Zsig.col(i) - z_pred;
-    while (z_diff(1) >  M_PI) z_diff(1) -= 2. * M_PI;
-    while (z_diff(1) < -M_PI) z_diff(1) += 2. * M_PI;
+    while (z_diff(1) >=  M_PI) z_diff(1) -= 2. * M_PI;
+    while (z_diff(1) <  -M_PI) z_diff(1) += 2. * M_PI;
     S += weights_(i) * z_diff * z_diff.transpose();
   }
 
@@ -292,16 +293,16 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
   VectorXd z = meas_package.raw_measurements_;
   MatrixXd Tc = MatrixXd(n_x_, n_z);
 
-  Tc.fill(0.0);
+  Tc.fill(0.);
 
   for (int i = 0; i < 2 * n_aug_ + 1; i++)
   {
     VectorXd x_diff = Xsig_pred_.col(i) - x_;
     VectorXd z_diff = Zsig.col(i) - z_pred;
-    while (x_diff(3) >  M_PI) x_diff(3) -= 2. * M_PI;
-    while (x_diff(3) < -M_PI) x_diff(3) += 2. * M_PI;
-    while (z_diff(1) >  M_PI) z_diff(1) -= 2. * M_PI;
-    while (z_diff(1) < -M_PI) z_diff(1) += 2. * M_PI;
+    while (x_diff(3) >=  M_PI) x_diff(3) -= 2. * M_PI;
+    while (x_diff(3) <  -M_PI) x_diff(3) += 2. * M_PI;
+    while (z_diff(1) >=  M_PI) z_diff(1) -= 2. * M_PI;
+    while (z_diff(1) <  -M_PI) z_diff(1) += 2. * M_PI;
     Tc += weights_(i) * x_diff * z_diff.transpose();
   }
 
@@ -309,8 +310,10 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
   K = Tc * S.inverse();
 
   VectorXd z_diff = z - z_pred;
-  while (z_diff(1) >  M_PI) z_diff(1) -= 2. * M_PI;
-  while (z_diff(1) < -M_PI) z_diff(1) += 2. * M_PI;
+  while (z_diff(1) >=  M_PI) z_diff(1) -= 2. * M_PI;
+  while (z_diff(1) <  -M_PI) z_diff(1) += 2. * M_PI;
   x_ += K * z_diff;
+  while (x_(3) >  M_PI) x_(3) -= 2. * M_PI;
+  while (x_(3) < -M_PI) x_(3) += 2. * M_PI;
   P_ -= K * S * K.transpose(); 
 }
